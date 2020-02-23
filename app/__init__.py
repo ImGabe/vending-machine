9b2916cf93fd3b8f988cd176693cbbb14fbfbe31
gabe <gabrielpmonte@hotmail.com>
2020-02-23 19:42:36 -0300

    fix PREFIX var

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,4 @@ from boto.s3.connection import S3Connection
 from discord.ext import commands
 
 
-client = commands.Bot(command_prefix=os.environ['TOKEN'])
+client = commands.Bot(command_prefix=os.environ['PREFIX'])
